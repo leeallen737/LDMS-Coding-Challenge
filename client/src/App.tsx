f21f369d3aa71687a739050929fc4c8fa117fa29
Lee J Allen <leeallen737@gmail.com>
2022-05-12 17:49:05 +0100

    installed sass. Added notes with some basic sass styling. Used a .map to produce a list of notes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import "./App.css";
+import React from 'react'
+import "./App.scss";
 // const { createProxyMiddleware } = require('http-proxy-middleware');
 function App() {
   // const apiProxy = createProxyMiddleware({
@@ -17,10 +18,46 @@ function App() {
   //   }  
 
   //   myFunction()
+  const [myNotes, updateMyNotes] = React.useState([
+    {
+      "id": 29,
+      "createdAt": "2021-07-17T18:04:38.040Z",
+      "user": "Hoyt Braun",
+      "note": "Sit iusto odit amet itaque sequi error laudantium fugit aperiam accusamus et mollitia est et necessitatibus iusto maxime sunt sed incidunt ut saepe quidem aspernatur modi consectetur illum qui vero."
+    },
+    {
+      "id": 30,
+      "createdAt": "2021-07-17T18:04:38.040Z",
+      "user": "David Braun",
+      "note": "Sit iusto odit amet itaque sequi error laudantium fugit aperiam accusamus et mollitia est et necessitatibus iusto maxime sunt sed incidunt ut saepe quidem aspernatur modi consectetur illum qui vero."
+    },
+    {
+      "id": 31,
+      "createdAt": "2018-07-17T18:04:38.040Z",
+      "user": "Jessica Braun",
+      "note": "Sit iusto odit amet itaque sequi error laudantium fugit aperiam accusamus et mollitia est et necessitatibus iusto maxime sunt sed incidunt ut saepe quidem aspernatur modi consectetur illum qui vero."
+    },
+    {
+      "id": 32,
+      "createdAt": "2019-07-17T18:04:38.040Z",
+      "user": "Stacey Braun",
+      "note": "Sit iusto odit amet itaque sequi error laudantium fugit aperiam accusamus et mollitia est et necessitatibus iusto maxime sunt sed incidunt ut saepe quidem aspernatur modi consectetur illum qui vero."
+    }
+  ])
+
 
   return (
     <div className="App">
-      
+      {myNotes.map((note) => {
+          return (
+          
+          <div key={note.id} className="my-note">
+          <h3>{note.createdAt}</h3>
+          <h3>{note.user}</h3>
+          <p>{note.note}</p>
+          </div>
+          )
+        })}
     </div>
   );
 }

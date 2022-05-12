34e5896feff3dffe438615337702dcbb42de6e5d
Lee J Allen <leeallen737@gmail.com>
2022-05-12 17:25:51 +0100

    Added API but CORS is in effect. Tried to mitgate without success. Left code commented for now, will add static data for the time being

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,23 @@
 import "./App.css";
-
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 function App() {
+  // const apiProxy = createProxyMiddleware({
+  //   target: 'http://localhost:8080',
+  //   changeOrigin: true,
+    
+  // });
+  // const myFunction = async () => {
+  //   const response = await fetch(
+  //     "http://localhost:8080/api/notes",
+  //     apiProxy
+  //   );
+  //   let data = await response.json()
+  //   console.log(data)
+    
+  //   }  
+
+  //   myFunction()
+
   return (
     <div className="App">
       
